b1ca488ecfd333d4f5fa1ec2a7912114ed5c1eb4
Stephanie Arcusa ~ Graduate Research Assistant <sha59@wind.nauhpc>
2020-07-22 08:18:19 -0700

    turned into function

--- runBrokenStick_null.R
+++ runBrokenStick_null.R
@@ -1,52 +1,49 @@
-## Written by Hannah Kolus, 09/04/2018 
-## Runs broken stick analysis using synthetic data across all relevant (flagged) proxy records
-
-library(geoChronR)
-library(segmented)
-library(lipdR)
-
-setwd('/Users/hannah/Documents/Arctic Group/Proxy analysis/forGitHub/4ka')
-source('gaussianize.R')
-source('brokenStick.R')
-load('BS_results_newLiPD.RData')
-
-numIt = 1000
-
-for (i in 1:length(TS_BS)) {
+BrokenStick_null <- function(data_in, param){
   
-  print(paste0('RECORD ', i))
+  TS_BS <- data_in
   
-  TS_BS[[i]]$null_brk_pts = list()
-  TS_BS[[i]]$null_brk_ptsErr = list()
-  
-  synthDat = createSyntheticTimeseries(TS_BS[[i]]$age, TS_BS[[i]]$paleoData_values, nens = numIt)
-
-  # run the broken stick code for all iterations
-  for (it in 1:numIt) {
-    print(paste0('ITERATION ', it))
+  for (i in 1:length(TS_BS)) {
     
-    results = iterativeBrokenStick(TS_BS[[i]]$age, synthDat[,it], plot.opt = F)
-    TS_BS[[i]]$null_brk_pts[[it]] = results$cp
-    TS_BS[[i]]$null_brk_ptsErr[[it]] = results$cp.se
+    print(paste0('RECORD ', i))
+    
+    TS_BS[[i]]$null_brk_pts = list()
+    TS_BS[[i]]$null_brk_ptsErr = list()
+    
+    tryCatch({synthDat = createSyntheticTimeseries(TS_BS[[i]]$age, 
+                                         TS_BS[[i]]$paleoData_values, 
+                                         nens = param$numIt)
+    
+    # run the broken stick code for all iterations
+    for (it in 1:param$numIt) {
+      print(paste0('ITERATION ', it))
+      
+      results = iterativeBrokenStick(TS_BS[[i]]$age, synthDat[,it], 
+                                     plot.opt = F)
+      
+      TS_BS[[i]]$null_brk_pts[[it]] = results$cp
+      TS_BS[[i]]$null_brk_ptsErr[[it]] = results$cp.se
+    }}, 
+    error = function(e){cat("Error:", conditionMessage(e), 
+                            paste0(" and had to skip record", i))})
+    
   }
   
-} # end loop thru records
-
-save(TS_BS, file = 'BS_results_plusNull_newLiPD.RData')
-
-## ---------------------- HISTOGRAM PLOT ---------------------- ##
-
-histBreaks = seq(100, 11500, by = 200)
-yrs = seq(300, 11500, by = 600)
-yrStr = yrs / 1000
-
-p=ggplot() + geom_histogram(aes(breakPts), breaks = yrs)
-pg = ggplot_build(p)
-densityBS = pg$data[[1]]$density
-xvals = pg$data[[1]]$x
-
-ggplot() + geom_col(aes(x = xvals, y = densityBS)) + 
-  xlab('Event years [BP]') + ylab('Density of events') + 
-  ggtitle('Broken Stick Results (simple plot)')
-
-
+  save(TS_BS, file = 'BS_results_plusNull.RData')
+  
+  histBreaks = seq(100, 11500, by = 200)
+  yrs = seq(300, 11500, by = 600)
+  yrStr = yrs / 1000
+  
+  p = ggplot() + geom_histogram(aes(histBreaks), breaks = yrs) # breakPts
+  pg = ggplot_build(p)
+  densityBS = pg$data[[1]]$density
+  xvals = pg$data[[1]]$x
+  
+  s  <- ggplot() + geom_col(aes(x = xvals, y = densityBS)) + 
+    xlab('Event years [BP]') + ylab('Density of events') + 
+    ggtitle('Broken Stick Results (simple plot)')
+  
+  output  <- list(TS_BS,s)
+  return(output)
+  
+}
