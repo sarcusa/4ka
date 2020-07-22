63d52126e37f7682e06e97463a6392dd69b46cc1
Stephanie Arcusa ~ Graduate Research Assistant <sha59@wind.nauhpc>
2020-07-22 08:19:31 -0700

    bash script to run make

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --job-name="Drake_test"
+#SBATCH --output="/home/sha59/4ka.out"
+#SBATCH -n 64
+#SBATCH --time=1000:00
+#SBATCH --cpus-per-task=1 
+#SBATCH --mem=64000
+#SBATCH --mail-type=ALL
+
+module load R
+srun Rscript make.R
