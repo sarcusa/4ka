9ca5ed0cfb2bd02651da559103b92237b01195ad
Stephanie Arcusa ~ Graduate Research Assistant <sha59@wind.nauhpc>
2020-07-22 10:00:28 -0700

    added cpus

--- plan_trendchanges.R
+++ plan_trendchanges.R
@@ -11,17 +11,17 @@ TrendChanges <- function(input_data, input_param, input_var){
   #print("calling workers")
   #future::plan(cluster, workers = cl)
   #analysis_3b = future({BrokenStick_null(analysis_3a[[1]], input_param)},
-                       lazy = TRUE)
+  #                     lazy = TRUE)
   #future::plan(sequential)
   #print("going back to sequential")
-  
+  analysis_3b = BrokenStick_null(analysis_3a, input_param)
 
 
-  analysis_3c = spatialBrokenStick(analysis_3b[[1]], input_param, 
+  analysis_3c = spatialBrokenStick(analysis_3b, input_param, 
                                    climateVar = input_var)
   
     
-  analysis_3d = hist_BS(data_in = analysis_3b[[1]], param = input_param,
+  analysis_3d = hist_BS(data_in = analysis_3b, param = input_param,
                         climateVar = input_var)
   
   out_3b = {
