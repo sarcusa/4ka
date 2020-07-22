2baa791e132977a6cafe8dc0740061bdd0da9cf5
Stephanie Arcusa ~ Graduate Research Assistant <sha59@wind.nauhpc>
2020-07-22 08:18:37 -0700

    create new plan

---
+++ plan_plotting.R
@@ -0,0 +1,14 @@
+Plotting <- function(analysis1, analysis2, analysis3, input_param, input_var){
+  
+  plots_1 = histogram_net(EX = analysis1, MS = analysis2, 
+                          BS = analysis3, param = input_param)
+  
+  plots_2 = ProxyMap(data_MS = analysis2[[2]], data_EX = analysis1[[2]], 
+                     param = input_param, climate_Var = input_var)
+  
+  output  <- list(plots_1[[1]], plots_1[[2]], plots_2[[1]], plots_2[[2]])
+  
+  return(output)
+  
+}
+
