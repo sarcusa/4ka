41f2470d16f2d3effade7e2d8213fae0565a603a
Stephanie Arcusa ~ Graduate Research Assistant <sha59@rain.nauhpc>
2020-07-30 08:02:20 -0700

    added a foreach parallelization for the brockenstick_null iteration loop

--- runBrokenStick_null.R
+++ runBrokenStick_null.R
@@ -1,40 +1,50 @@
 BrokenStick_null <- function(data_in, param){
   
+  print("Running brockenStick_null now")
+  
   TS_BS <- data_in[[1]]
   
+  registerDoParallel(cores = 4)
+  
   for (i in 1:length(TS_BS)) {
-    
+  
     print(paste0('RECORD ', i))
     
     TS_BS[[i]]$null_brk_pts = list()
     TS_BS[[i]]$null_brk_ptsErr = list()
     
-    tryCatch({synthDat = createSyntheticTimeseries(TS_BS[[i]]$age, 
-                                         TS_BS[[i]]$paleoData_values, 
-                                         nens = param$numIt)
+    synthDat = try(createSyntheticTimeseries(TS_BS[[i]]$age,
+                                         TS_BS[[i]]$paleoData_values,
+                                         nens = param$numIt))
+    if(class(synthDat) == "try-error"){
+      
+      print(paste0("Had to skip record = ",TS_BS[[i]]$dataSetName))
+      TS_BS[[i]]$null_brk_pts = rep(list(NA), param$numIt)
+      TS_BS[[i]]$null_brk_ptsErr = rep(list(NA), param$numIt)
+      
+    }else{
+    cp.out <-foreach(it=1:param$numIt,
+                     .verbose=TRUE,.errorhandling = "pass") %dopar% {
+                       results = iterativeBrokenStick(TS_BS[[i]]$age, 
+                                                      synthDat[,it], 
+                                                      plot.opt = F)
+                     
+                       return(list(cp = results$cp, cp.se = results$cp.se))
+                     }    
+    stopImplicitCluster()
+    }
     
-    # run the broken stick code for all iterations
-    for (it in 1:param$numIt) {
-      print(paste0('ITERATION ', it))
-      
-      results = iterativeBrokenStick(TS_BS[[i]]$age, synthDat[,it], 
-                                     plot.opt = F)
-      
-      TS_BS[[i]]$null_brk_pts[[it]] = results$cp
-      TS_BS[[i]]$null_brk_ptsErr[[it]] = results$cp.se
-    }}, 
-    error = function(e){cat("Error:", conditionMessage(e), 
-                            paste0(" and had to skip record", i))})
+    TS_BS[[i]]$null_brk_pts = lapply(cp.out, `[[`, 1)
+    TS_BS[[i]]$null_brk_ptsErr = lapply(cp.out, `[[`, 2)
     
   }
-  
   save(TS_BS, file = 'BS_results_plusNull.RData')
   
   histBreaks = seq(100, 11500, by = 200)
   yrs = seq(300, 11500, by = 600)
   yrStr = yrs / 1000
   
-  p = ggplot() + geom_histogram(aes(histBreaks), breaks = yrs) # breakPts
+  p = ggplot() + geom_histogram(aes(histBreaks), breaks = yrs) # histBreaks used to be breakPts
   pg = ggplot_build(p)
   densityBS = pg$data[[1]]$density
   xvals = pg$data[[1]]$x
