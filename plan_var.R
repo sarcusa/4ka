312f34fd7c1147f49cb30f10eeb1d6fe67f1b8bf
Stephanie Arcusa ~ Graduate Research Assistant <sha59@rain.nauhpc>
2020-07-30 08:00:50 -0700

    added n cores choice

--- plan_var.R
+++ plan_var.R
@@ -18,24 +18,24 @@ my_plan <- drake_plan(
                          input_var = climate_var),
                                          transform = cross(
                                            climate_var = c("M", "T", "all")),
-                      resources = list(cores = 2)),
+                      resources = list(cores = 4)),
   
   
   analysis_2 = target(MeanShift(input_data = data, input_param = parameters,
                          input_var = climate_var),
                       transform = cross(climate_var = c("M", "T", "all")),
-                      resources = list(cores = 2)),
+                      resources = list(cores = 4)),
   
   analysis_3 = target(TrendChanges(input_data = data,input_param = parameters,
                             input_var = climate_var), 
-                      transform = cross(climate_var = c("M", "T", "all")),
-                      resources = list(cores = 6)),
+                      transform = cross(climate_var = c("M", "T")),
+                      resources = list(cores = 16)),
   
   results_1 = target(Plotting(analysis1 = analysis_1, analysis2 = analysis_2,
                               analysis3 = analysis_3, 
                               input_param = parameters, input_var = climate_var),
-                     transform = map(analysis_1, analysis_2, analysis_3)),
-                      
+                     transform = map(analysis_1, analysis_2, analysis_3),
+                     resources = list(cores = 1)),
   
   report  = target(rmarkdown::render(
     knitr_in("report.Rmd"),
