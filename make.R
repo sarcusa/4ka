dfbcfb95714ad629520404b213caefba1ff36640
Stephanie Arcusa ~ Graduate Research Assistant <sha59@rain.nauhpc>
2020-07-30 07:59:51 -0700

    remove fluff

--- make.R
+++ make.R
@@ -15,12 +15,11 @@ source('plan_var.R') #, local = envir
 
 #file.exists("report.Rmd")
 
-#drake_hpc_template_file("slurm_batchtools.tmpl")
-#future::plan(list(batchtools_slurm,multiprocess), template = "slurm_batchtools.tmpl")
+#future::plan(future::multisession, workers = 4)
 future::plan(batchtools_slurm, template = "/home/sha59/4ka/slurm_batchtools.tmpl")
 
 # Download necessary data
-set.seed(1)
+#set.seed(1)
 external_data  = readLipd(file_in("http://lipdverse.org/HoloceneAbruptChange/0_9_0/HoloceneAbruptChange0_9_0.zip"))
 print("data downloaded")
 data_sub = list.sample(external_data, size = 50, replace = F)
@@ -30,18 +29,5 @@ print("TS extracted")
 
 #vis_drake_graph(my_plan)
 
-#make(
-#  envir$my_plan,
-#  parallelism = "future",
-#  jobs = 4,
-#  console_log_file = "drake.log",
-#  lock_envir = FALSE,
-#  envir = envir
-#)
-
-#make(plan, max_expand = 2)
-#vis_drake_graph(plan)
-# plot(plan)
-
 make(my_plan, lock_envir = FALSE, lock_cache = FALSE, 
-     parallelism = "future")
+     parallelism = "future", jobs = 4)
