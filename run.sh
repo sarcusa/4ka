153c5a11ab7c2c55a8f7a3616b977a68406d83e9
Stephanie Arcusa ~ Graduate Research Assistant <sha59@rain.nauhpc>
2020-07-30 08:01:35 -0700

    removed some choices:

--- run.sh
+++ run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name="Drake_test"
-#SBATCH --output="/home/sha59/4ka.out"
-#SBATCH -n 64
 #SBATCH --time=1000:00
 #SBATCH --mem=64000
 #SBATCH --mail-type=ALL
 
 module load R
 srun Rscript make.R
+
+
