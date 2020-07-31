058602edfb7b051efcc3baf659c8af9f7bb29ce8
Stephanie Arcusa ~ Graduate Research Assistant <sha59@wind.nauhpc>
2020-07-31 14:06:48 -0700

    fixed foreach issue

--- spatialExcursion_direction_null.R
+++ spatialExcursion_direction_null.R
@@ -1,17 +1,22 @@
 spatialExcursion_null  <-  function(data_in, param, climateVar){
   
-  exDir = file.path(createPaths(), 'excursion')
-  dir.create(file.path(exDir, 'spatial_EX_M'))
-  dir.create(file.path(exDir, 'spatial_EX_T'))
-  
-  #TS_EX_orig = data_in
+  TS_EX_orig = data_in
   
   for (y in 1:length(param$eventYrs)) {
     
+    #rm(list = ls())
+    
+    exDir = file.path(createPaths(), 'excursion')
+    dir.create(file.path(exDir, 'spatial_EX_M'))
+    dir.create(file.path(exDir, 'spatial_EX_T'))
+    datDir = file.path(createPaths(), 'RData')    
+    
     eventYr = param$eventYrs[y]
     
+    list2env(loading(file.path(datDir, paste0('EX_results_plusNull_complete_', eventYr/1000, '.RData'))),envir=.GlobalEnv)
+    
     #TS_EX = TS_EX_orig
-    TS_EX = data_in
+    TS_EX = data_EX
     
     for (i in 1:length(TS_EX)) {
       
