60854db89aa3d174c87066e75e5d9560870671eb
Stephanie Arcusa ~ Graduate Research Assistant <sha59@wind.nauhpc>
2020-07-31 14:06:04 -0700

    added foreach

--- runExcursion_null.R
+++ runExcursion_null.R
@@ -1,17 +1,20 @@
 Excursion_null <- function(data_in, param){
-  for (y in 1:length(eventYrs)) {
+  
+  dataDir = file.path(createPaths(), 'RData')
+  
+  for (y in 1:length(param$eventYrs)) {
     
-    dataDir = file.path(createPaths(), 'RData')
-    
+    remove(data_EX)
     event_yr = param$eventYrs[y]
     print(paste('Event year:', event_yr))
     
     # Load in the TS structure of excursion results, plus event_window and ref_window
+    list2env(loading(file.path(dataDir, paste0('EX_results', event_yr/1000, '.RData'))),envir=.GlobalEnv)
     #load(file.path(dataDir, paste0('EX_results', event_yr/1000, '.RData')))
-    data_EX = filterTs(data_in, 'statusEX == 1')
+    data_EX = filterTs(data_EX, 'statusEX == 1')
     
     for (i in 1:length(data_EX)) {
-      
+            
       print(paste0('RECORD ', i))
       
       data_EX[[i]]$null_events = list()
@@ -22,11 +25,17 @@ Excursion_null <- function(data_in, param){
         ageInds = NA
       }
       
-      synthDat = try(createSyntheticTimeseries(data_EX[[i]]$age, data_EX[[i]]$paleoData_values,nens = param$numIt, index.to.model = ageInds), silent = T)
+      synthDat = try(createSyntheticTimeseries(data_EX[[i]]$age, 
+                                               data_EX[[i]]$paleoData_values,
+                                               nens = param$numIt, 
+                                               index.to.model = ageInds), 
+                     silent = T)
       
       if (class(synthDat) == "try-error") {
         print(paste('Try error, running instead with method = ML'))
-        synthDat = try(synth_fun(data_EX[[i]]$age, data_EX[[i]]$paleoData_values, nens = param$numIt,index.to.model = ageInds))
+        synthDat = try(synth_fun(data_EX[[i]]$age, 
+                                 data_EX[[i]]$paleoData_values, 
+                                 nens = param$numIt,index.to.model = ageInds))
         
         if (class(synthDat) == "try-error") {
           print(paste('Try error again, skipping this record'))
@@ -34,16 +43,28 @@ Excursion_null <- function(data_in, param){
           next
         }
       }
+      registerDoParallel(cores = 16)
+      #for (it in 1:param$numIt) {
+      out <-foreach(it=1:param$numIt,
+                    .verbose=F,.errorhandling = "pass") %dopar% { 
+                      
+                      #print(paste('Iteration', it))
+                      
+                      results = EX_fun(data_EX[[i]]$age, synthDat[,it], 
+                                       event_yr = event_yr, 
+                                       event_window = param$event_window, 
+                                       ref_window = param$ref_window)
+                      
+                      return(list(null_2 = results[[2]], null_3 = results[[3]]))
+                    }
+      stopImplicitCluster()
+        
+      data_EX[[i]]$null_events = lapply(out, `[[`, 1)
+      data_EX[[i]]$null_events_dir = unlist(lapply(out, `[[`, 2))
       
-      for (it in 1:param$numIt) {
-        print(paste('Iteration', it))
-        
-        results = EX_fun(data_EX[[i]]$age, synthDat[,it], event_yr = event_yr, 
-                         event_window = param$event_window, ref_window = param$ref_window)
-        
-        data_EX[[i]]$null_events[[it]] = results[[2]]
-        data_EX[[i]]$null_events_dir[[it]] = results[[3]]
-      } 
+      #data_EX[[i]]$null_events[[it]] = results[[2]]
+      #data_EX[[i]]$null_events_dir[[it]] = results[[3]]
+       
       
     } # end loop thru records
     event_window = param$event_window
