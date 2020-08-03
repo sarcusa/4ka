e3013d2dab215c4b108a9d3c5d71e80610b17af2
Stephanie Arcusa ~ Graduate Research Assistant <sha59@rain.nauhpc>
2020-08-03 13:14:37 -0700

    disbanded

--- plan_plotting.R
+++ plan_plotting.R
@@ -1,9 +1,9 @@
-Plotting <- function(analysis1, analysis2, analysis3, input_param, input_var){
+Plotting <- function(prep1,prep2,analysis1, analysis2, analysis3,input_param, input_var){
   
   plots_1 = histogram_net(EX = analysis1, MS = analysis2, 
                           BS = analysis3, param = input_param)
   
-  plots_2 = ProxyMap(data_MS = analysis2[[2]], data_EX = analysis1[[2]], 
+  plots_2 = ProxyMap(data_MS = prep2, data_EX = prep1, 
                      param = input_param, climate_Var = input_var)
   
   output  <- list(plots_1[[1]], plots_1[[2]], plots_2[[1]], plots_2[[2]])
