262769d81cd03cc4323ad51bce7e28dc5d1c9a33
Stephanie Arcusa ~ Graduate Research Assistant <sha59@rain.nauhpc>
2020-08-03 13:13:59 -0700

    small changes

--- plan_excursion.R
+++ plan_excursion.R
@@ -1,23 +1,9 @@
 Excursion <- function(input_data,input_param, input_var){
   
-  analysis_1a = applyEX(data_in = input_data, param = input_param)
-  
-  out1a = {
-    datPath = file.path(createPaths(), 'RData', 'Excursion.RData')
-    save(analysis_1a, file = datPath)
-  }
-  
-  analysis_1b = Excursion_null(data_in = analysis_1a, param = input_param)
-  
-  out1b = {
-    datPath = file.path(createPaths(), 'RData', 'Excursion_plusnull.RData')
-    save(analysis_1b, file = datPath)
-  }
-  
-  analysis_1c = spatialExcursion_null(data_in = analysis_1b, 
+  analysis_1c = spatialExcursion_null(data_in = input_data, 
                                       param = input_param, climateVar = input_var)
   
-  analysis_1d = histEX(data_in = analysis_1b, param = input_param, 
+  analysis_1d = histEX(data_in = input_data, param = input_param, 
                        climateVar = input_var)
   
   
@@ -29,7 +15,7 @@ Excursion <- function(input_data,input_param, input_var){
     save(analysis_1e, file = datPath)
   }
   
-  output = list(analysis_1e, analysis_1b, analysis_1d)  
+  output = list(analysis_1e, analysis_1d)  
   
   return(output)
   
