c58e80f965f8fbb03b0d9ad56e90cdb69f0257a3
Stephanie Arcusa ~ Graduate Research Assistant <sha59@wind.nauhpc>
2020-08-07 15:31:14 -0700

    fixed bug

--- plan_prep_meanshift.R
+++ plan_prep_meanshift.R
@@ -1,18 +1,22 @@
 MeanShift_prep <- function(input_data,input_param){
   
+  print("Start apply meanshift function")
   analysis_2a = applyMS(data_in = input_data)
   
   out2a = {
     datPath = file.path(createPaths(), 'RData', 'MeanShift.RData')
     save(analysis_2a, file = datPath)
   }
+  print("Completed apply meanshift function")
   
+  print("Start meanshift null function")
   analysis_2b = MeanShift_null(data_in = analysis_2a, param = input_param)
   
   out_2b = {
     datPath = file.path(createPaths(), 'RData', 'MS_results_plusNull_complete.RData')
     save(analysis_2b, file = datPath)
   }
+  print("Completed meanshift null function")
   
   return(analysis_2b)
   
