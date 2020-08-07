a7638ec0471a1943aa68e765c1094025a7bc9ec6
Stephanie Arcusa ~ Graduate Research Assistant <sha59@wind.nauhpc>
2020-08-07 15:31:32 -0700

    fixed bug

--- plan_prep_trendchanges.R
+++ plan_prep_trendchanges.R
@@ -1,18 +1,23 @@
 TrendChanges_prep <- function(input_data, input_param){
   
+  print("Start brocken stick function")
   analysis_3a = BrokenStick(input_data, input_param)
   
   out3a = {
     datPath = file.path(createPaths(), 'RData', 'TrendChanges.RData')
     save(analysis_3a, file = datPath)
   }
+  print("completed brocken stick function")
   
+  print("start brockenstick null function")
   analysis_3b = BrokenStick_null(analysis_3a, input_param)
   
   out_3b = {
     datPath = file.path(createPaths(), 'RData', 'BS_results_plusNull_complete.RData')
     save(analysis_3b, file = datPath)
   }
+  print("completed brockenstick null function")
 
+  
   return(analysis_3b)
 }
