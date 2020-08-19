6bea1dc47e5b818a9ab2aeacdfde9cdb4909381b
Stephanie Arcusa ~ Graduate Research Assistant <sha59@wind.nauhpc>
2020-08-19 15:18:36 -0700

    added code to calculate the null direction like in runBrokenStick - this was the reason that the null would plot as zero i.e. it was never calculated

--- runBrokenStick_null.R
+++ runBrokenStick_null.R
@@ -2,62 +2,93 @@ BrokenStick_null <- function(data_in, param){
   
   print("Running brockenStick_null now")
   
-  TS_BS <- data_in[[1]]
+  TS_BS <- data_in
+  skipped_records  <- vector()
+  
+  #plan(cluster)
   
   for (i in 1:length(TS_BS)) {
-  
-    print(paste0('RECORD ', i))
+  #  for (i in 1:50) {
+    #print(paste0('RECORD ', i))
     
     TS_BS[[i]]$null_brk_pts = list()
     TS_BS[[i]]$null_brk_ptsErr = list()
     
+    # added on 18-08-20 SA
+    TS_BS[[i]]$null_brk_dirs = list()
+    
     synthDat = try(createSyntheticTimeseries(TS_BS[[i]]$age,
                                          TS_BS[[i]]$paleoData_values,
-                                         nens = param$numIt))
+                                         nens = param$numIt), silent = T)
     if(class(synthDat) == "try-error"){
       
-      print(paste0("Had to skip record = ",TS_BS[[i]]$dataSetName))
-      TS_BS[[i]]$null_brk_pts = rep(list(NA), param$numIt)
-      TS_BS[[i]]$null_brk_ptsErr = rep(list(NA), param$numIt)
-      
-    }else{
+      synthDat = try(createSyntheticTimeseries(TS_BS[[i]]$age,
+                                               TS_BS[[i]]$paleoData_values,
+                                               nens = param$numIt), silent = T)
+      if(class(synthDat) == "try-error"){
+        
+        print(paste0("Had to skip record = ",TS_BS[[i]]$dataSetName))
+        skipped_records[i]  <- TS_BS[[i]]$dataSetName
+        TS_BS[[i]]$useBS = -1
+        
+        next 
+      }
+    }
     
-      registerDoParallel(cores = param$ncores)
+    registerDoParallel(cores = param$ncores)
+      
+    cp  <- list()
+    cp.se <- list()
+    brk_dirs  <- list()
       
     cp.out <-foreach(it=1:param$numIt,
-                     .verbose=F,.errorhandling = "pass") %dopar% {
+                     .verbose=F,.errorhandling = "pass") %do% {
                        
-                       print(paste0('BS null ITERATION ', it))
-                       
+                       #print(paste0('BS null ITERATION ', it))
+                       #for (it in 1:param$numIt) {
                        results = iterativeBrokenStick(TS_BS[[i]]$age, 
                                                       synthDat[,it], 
                                                       plot.opt = F)
-                     
-                       return(list(cp = results$cp, cp.se = results$cp.se))
+                     #  TS_BS[[i]]$null_brk_pts[[it]] = results$cp
+                     #  TS_BS[[i]]$null_brk_ptsErr[[it]] = results$cp.se
+                     #}
+                       
+                       # added on 18-08-20 SA
+                       if(is.character(results$cp) | is.na(results$cp)){
+                         cp = NA 
+                         cp.se = NA
+                         brk_dirs = NA
+                       
+                       }else{
+                         
+                         cp = results$cp
+                         cp.se = results$cp.se
+                         
+                         slopes = results$o$coefficients[3:(length(results$cp)+2)]
+                         brk_dirs = ifelse(slopes > 0, 1, -1)
+                         
+                         
+                       }
+                                              
+                       return(list(cp = cp, 
+                                   cp.se = cp.se, 
+                                   brk_dirs = brk_dirs))
                      }    
     stopImplicitCluster()
-    }
     
-    TS_BS[[i]]$null_brk_pts = lapply(cp.out, `[[`, 1)
-    TS_BS[[i]]$null_brk_ptsErr = lapply(cp.out, `[[`, 2)
+    
+    TS_BS[[i]]$null_brk_pts = lapply(cp.out, `[[`, "cp")
+    TS_BS[[i]]$null_brk_ptsErr = lapply(cp.out, `[[`, "cp.se")
+    # added on 18-08-20 SA
+    TS_BS[[i]]$null_brk_dirs = lapply(cp.out, `[[`, "brk_dirs")
+    
     
   } #end of for loop
-  save(TS_BS, file = 'BS_results_plusNull.RData')
   
-  histBreaks = seq(100, 11500, by = 200)
-  yrs = seq(300, 11500, by = 600)
-  yrStr = yrs / 1000
+  fileName = file.path(mainDir, 'RData', 'BS_results_plusNull.RData')
+  save(TS_BS, file = filename)
+  write.table(skipped_records, file = file.path(mainDir,"skipped_records_BS.txt"))
   
-  p = ggplot() + geom_histogram(aes(histBreaks), breaks = yrs) # histBreaks used to be breakPts
-  pg = ggplot_build(p)
-  densityBS = pg$data[[1]]$density
-  xvals = pg$data[[1]]$x
-  
-  s  <- ggplot() + geom_col(aes(x = xvals, y = densityBS)) + 
-    xlab('Event years [BP]') + ylab('Density of events') + 
-    ggtitle('Broken Stick Results (simple plot)')
-  
-  output  <- list(TS_BS,s)
-  return(output)
+  return(TS_BS)
   
 }
