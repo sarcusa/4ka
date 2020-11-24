d2d0c5413ad14ade8b68fcd6f54d882a19c75b85
Stephanie Arcusa ~ Graduate Research Assistant <sha59@wind.nauhpc>
2020-11-24 14:43:12 -0700

    added seed

--- plan_var.R
+++ plan_var.R
@@ -10,37 +10,39 @@ my_plan <- drake_plan(
                     eventWindow = eventWindow, 
                     CName = CName, CVers = CVers, 
 		    eventDetector = eventDetector, OutDat = OutDat,
-	            ncores = ncores, maxDiff = maxDiff), hpc = FALSE),
+	            ncores = ncores, maxDiff = maxDiff, det = det), hpc = FALSE),
 
   data = target(plan_prep(param = parameters), hpc = FALSE),
 
-  map = target(plot_sites(data), hpc = FALSE),
+  #map = target(plot_sites(data), hpc = FALSE),
 
   #prep_1 = target(Excursion_prep(input_data = data,input_param = parameters),
-  #                resources = list(cores = 16)),  
+  #                resources = list(cores = 8)),  #was 16
   
   #analysis_1 = target(Excursion(input_data = prep_1,input_param = parameters,
   #                       input_var = climate_var),
   #                    transform = cross(
-  #                      climate_var = c("T","M","All")),
-  #                   resources = list(cores = 16))
+  #                      climate_var = c("T","M")),
+  #                   resources = list(cores = 8)) #was 16
   #,
   
   prep_2 = target(MeanShift_prep(input_data = data,input_param = parameters),
-                  resources = list(cores = 32)), 
+                  resources = list(cores = 8)), #was 32
   
   analysis_2 = target(MeanShift(input_data = prep_2, input_param = parameters,
                          input_var = climate_var),
-                      transform = cross(climate_var = c("T","M","All")),
-                      resources = list(cores = 16))
+                      transform = cross(climate_var = c("T","M")),
+                      resources = list(cores = 8)) #was 32
   #,
   
   #prep_3a = target(TrendChanges_prep1(input_data = data,input_param = parameters),
-  #                resources = list(cores = 16)),
+  #                resources = list(cores = 16))
+  #,
   
   #prep_3b = target(TrendChanges_prep2(input_data = prep_3a,
   #                                    input_param = parameters),
-  #                 resources = list(cores = 32)),
+  #                 resources = list(cores = 32))
+  #,
   
   #test = target(BrokenStick_null_test(data_in = prep_3a, param = parameters),
   #              resources = list(cores = 16))
@@ -51,9 +53,11 @@ my_plan <- drake_plan(
   #                    resources = list(cores = 16)),
   
   #results_1 = target(ProxyMap(prep1 = prep_1, prep2 = prep_2,
-  #                            param = parameters, input_var = climate_var),
+  #                            param = parameters, input_var = climate_var,
+  #                   data_in = data),
   #                   hpc = FALSE,
-  #                   transform = cross(climate_var = c("T", "M", "All))),
+  #                   transform = cross(climate_var = c("T", "M")))
+  #,
   
   #results_2 = target(histogram_net(EX = analysis_1, MS = analysis_2, 
   #                                 BS = analysis_3, param = parameters), 
