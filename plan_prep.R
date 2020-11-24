c78740f4015f46e69559a510235abe7dd48bf3f1
Stephanie Arcusa ~ Graduate Research Assistant <sha59@wind.nauhpc>
2020-11-24 14:45:13 -0700

    changes

--- plan_prep.R
+++ plan_prep.R
@@ -4,7 +4,8 @@ plan_prep <- function(data_in = data_all, param){
   data_index  = inThisCompilation(TS = data_in,compName = param$CName, compVers = param$CVers)
   data_comp = data_all[unlist(data_index)]
   climate_index = climate_indices(data_comp)
-  data = processLiPD(data_in = data_comp, climInds = climate_index)
+  data = processLiPD(data_in = data_comp, climInds = climate_index, 
+                     detrend = param$det)
   out = {
     datPath = file.path(createPaths(), 'RData', param$OutDat)
     save(data, file = datPath)
