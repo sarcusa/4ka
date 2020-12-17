4b3b448641a36daf562cc59c5311da2f31fa7bc5
Stephanie Harmonie Arcusa <sha59@wind.nauhpc>
2020-12-17 16:33:37 -0700

    temprarily added different partition

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#SBATCH --time=400:00 ##was 3000, then 1000
+#SBATCH --time=400:00 ##was 3000, then 1000, then 400
 #SBATCH --mem=64000
 #SBATCH --mail-type=ALL
+##SBATCH --partition=el6
 
 ##module load openmpi
 module load R
