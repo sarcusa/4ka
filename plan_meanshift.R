faba10e0e63f0922cc6f696ee9ad928989513a4b
Stephanie Arcusa ~ Graduate Research Assistant <sha59@rain.nauhpc>
2020-12-31 21:11:34 -0700

    separated spatial from temporal analysis

--- plan_meanshift.R
+++ plan_meanshift.R
@@ -1,7 +1,7 @@
 MeanShift <- function(input_data,input_param, input_var){
   
-  analysis_2c = spatialMeanShift_null(data_in = input_data, 
-                                      param = input_param, climateVar = input_var)
+  #analysis_2c = spatialMeanShift_null(data_in = input_data, 
+  #                                    param = input_param, climateVar = input_var)
   
   analysis_2d = histogram_MS(data_in = input_data, 
                              param = input_param, climateVar = input_var)
