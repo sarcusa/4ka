3c8791b049c010ff7b8bdae6ca225e4f47d29931
Stephanie Arcusa ~ Graduate Research Assistant <sha59@rain.nauhpc>
2020-12-31 21:12:56 -0700

    switched

--- set_parameters.R
+++ set_parameters.R
@@ -1,7 +1,7 @@
 # Set Parameters 
 
 #output_destination = dir
-output_destination = "/projects/pd_lab/sha59/4ka/Stability_test/J/" #Use dir for the same folder as the inputs. 
+output_destination = "/projects/pd_lab/sha59/4ka/Signif_test_MS_99_750/" #Use dir for the same folder as the inputs. 
 
 # Parameters common to all analyses
 
@@ -15,23 +15,23 @@ radius = 2000 # search radius, km for the spatial excursion
 climateVar = 'T' # Deprecated. M for moisture, T for temperature, All for all. 
 plotOpt = T # Plotting?
 ncores = 8 #was 128 #how many cores to run in parallel? Deprecated
-det = F #detrend records before running the analyses?
+det = FALSE #detrend records before running the analyses? Deprecated
 
 # Excursion
 
 event_yr = 3800 # Set the event year (select any year, this will change)
-event_window = 600 #(event_yr +/- event_window/2) as the event window. Was 600.
-ref_window = 500  # Set the duration of the reference windows. Was 500. 
+event_window = 500 #(event_yr +/- event_window/2) as the event window. Was 600.
+ref_window = 600  # Set the duration of the reference windows. Was 500. 
 resCriteria = 50 # Resolution criteria (yr) required to include record
 sigNum = 2 # Set how many sigmas below and above the mean define an excursion
 mainDir = createPaths() # do not change.
 
 # Mean shift & Trend Change
 
-maxDiff = 1000    # min segment length between change points. Was 1000. 
+maxDiff = 750    # min segment length between change points. Was 1000. 
 alpha = 0.05      # set confidence level for mean difference testing
 gaussOpt = F      # option to gaussianize values before analysis
 eventYr = 8200     # event year, yr BP. Placeholder.
-eventWindow = 199.9  # total event window = eventYr +/- eventWindow. Was 199.9
+eventWindow = 99.9  # total event window = eventYr +/- eventWindow. Was 199.9
 eventDetector = 'MS'   # Deprecated. MS for mean shift, BS for broken stick
 
